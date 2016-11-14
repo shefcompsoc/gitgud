3459ca28f8ccccd0fcfe67a0519ec53c873ce88b
Rob Ede <robjtede@gmail.com>
2016-11-14 18:43:25 +0000

    Test commit

---
+++ Bar.java
@@ -0,0 +1,5 @@
+class Bar {
+  public static void main(String[] args) {
+    new Foo().sayHello();
+  }
+}
--- Foo.java
+++ Foo.java
@@ -8,8 +8,4 @@ class Foo {
   public void sayHello () {
     System.out.println(this.message);
   }
-
-  public static void main(String[] args) {
-    new Foo().sayHello();
-  }
 }
